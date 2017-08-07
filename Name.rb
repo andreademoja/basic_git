fa65fac257543ec3430757f57ecc1b8542c25345
Andrea <andreademoja@gmail.com>
2017-08-07 14:23:13 +0200

    Asking the name

---
+++ Name.rb
@@ -0,0 +1,5 @@
+puts 'Hello man, what\'s your name?'
+name = gets.chomp
+puts 'Well, ' + name + ', and what is your second name?'
+name = gets.chomp
+puts 'Nice to meet you then, ' + name + ', take care!'
