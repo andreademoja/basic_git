194e84b1a595f9a77c8206cff7c8fdd9813c8b80
Andrea <andreademoja@gmail.com>
2017-08-07 21:40:36 +0200

    Playing with Ruby Branches

---
+++ Branch_test.rb
@@ -0,0 +1,9 @@
+puts 'Hi, i\'m Andrea, and you are?'
+name = gets.chomp
+puts 'Welcome ' + name + ', it\'s you first time in Italy?'
+input = gets.chomp
+if input == 'yes'
+  puts 'Well, i\'m gonna tell you what places you shoul visit'
+else
+  puts 'Alright than, so you already know what places you should visit'
+end
